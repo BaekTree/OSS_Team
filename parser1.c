b7336b9bc67a4971b7c1bd38815a4adfcfab4118
saintmin92 <tajiklee@naver.com>
2019-05-23 22:47:54 +0900

    object solved

--- parser1.c
+++ parser1.c
@@ -22,7 +22,6 @@ int main() {
     FILE *fp;
     const int maxLen = 256;
     char buffer[maxLen];
-    char buffer2[maxLen];
     char *data = (char *)malloc(maxLen * sizeof(char));
 
     //file open
@@ -51,7 +50,7 @@ int main() {
 
     // printf("The count is : \n%d\n", nos);
     // printf("The length of string is : %d\n", length);
-    printf("The string is : \n%s\n", data);
+    // printf("The string is : \n%s\n", data);
     fclose(fp);
 
     //dynamic structure array
@@ -59,7 +58,7 @@ int main() {
     // tArr = (tok_t*)malloc(1 * sizeof(tArr));
 
     //not dynamic
-    tok_t * tArr[50];
+    tok_t * tArr[40];
     // 구조체 포인터 배열 전체 크기에서 요소(구조체 포인터)의 크기로 나눠서 요소 개수를 구함
     for (int i = 0; i < sizeof(tArr) / sizeof(tok_t *); i++)    // 요소 개수만큼 반복
     {
@@ -69,7 +68,7 @@ int main() {
     int startCursor = 0;
     int endCursor;
     int tsize = 0;
-    int ntok = 0;
+    int ntok = 0; //mun of tok
 
     int cbcounter = 0;
     int sbcounter = 0;
@@ -91,20 +90,21 @@ int main() {
                         }
                     }
                     if (data[i+1] == ':'){
-                        if (cbcounter == 0 || sbcounter == 0) {
+                        if (cbcounter == 0 && sbcounter == 0) {
                             tsize = 1;
                             tArr[ntok]->start = startCursor;
                             tArr[ntok]->end = endCursor;
                             tArr[ntok]->size = tsize;
                             tsize = 0;
                             ntok++;
+                            i++;
                         }
                         else { // if it is in the object or array size +1
                             tsize++;
                         }                        
                     }
                     else {
-                        if (cbcounter == 0 || sbcounter == 0) {
+                        if (cbcounter == 0 && sbcounter == 0) {
                             tsize = 0;
                             tArr[ntok]->start = startCursor;
                             tArr[ntok]->end = endCursor;
@@ -127,7 +127,7 @@ int main() {
                 cbcounter--;
                 if (cbcounter == 0) {
                     tArr[ntok]->size = tsize; //
-                    tArr[ntok]->end = endCursor;
+                    tArr[ntok]->end = i; // +1?
                     i = tArr[ntok]->start + 1;
                     ntok++;
                     tsize = 0;
@@ -172,11 +172,6 @@ int main() {
     //key냐 value냐 인식 필요 (flag로? 아니면 :로?)
     //"가 스트링의 시작이면 ", 로 끝나는 단어를 만날 때까지 찾아야함 그러나 ", 앞에 \가 오면 안됨
     //"" , : 같은 문자 제거 (특문이랑 구분 필요)
-    /*[ { 만나면 ] } 찾아서 오브젝트화 X
-    일단 오브젝트를 만드는 방법?
-    이렇게 하면 { 안에  {가 있어도 계속해서 만들면서 내려가기 때문에 좋다}}
-    단 닫는 }를 만났을 때 거슬러 올라가면서 오브젝트를 개수를 카운트 하고 미완성된 오브젝트를 완성시켜줘야될 필요가 있다.
-    */
 
     printf("\nEnd of Program\n");
 
