07197f6914f8ac4e04101a9b3bf24989b9c9f13a
saintmin92 <tajiklee@naver.com>
2019-05-24 17:20:20 +0900

    variable naming

--- Sungmin/parser1.c
+++ Sungmin/parser1.c
@@ -27,7 +27,7 @@ int main() {
     //file open
     fp = fopen("test2.json", "r");
     int length = 0; //total length
-    int nos = 0; // number of strings
+    int num_of_str = 0; // number of strings
 
     // reading file by character
     int ch;
@@ -40,7 +40,7 @@ int main() {
             data = (char *) realloc(data, strlen(data) + strlen(buffer));
             strcat(data, buffer);
             memset(buffer, 0, sizeof buffer);
-            nos++;
+            num_of_str++;
             j = -1;
         }
     }
@@ -48,77 +48,77 @@ int main() {
     length++;
 
 
-    // printf("The count is : \n%d\n", nos);
+    // printf("The count is : \n%d\n", num_of_str);
     // printf("The length of string is : %d\n", length);
     // printf("The string is : \n%s\n", data);
     fclose(fp);
 
     //dynamic structure array
-    // struct tok_t *tArr;
-    // tArr = (tok_t*)malloc(1 * sizeof(tArr));
+    // struct tok_t *token_arr;
+    // token_arr = (tok_t*)malloc(50 * sizeof(token_arr));
 
     //not dynamic
-    tok_t * tArr[100];
+    tok_t * token_arr[100];
     // 구조체 포인터 배열 전체 크기에서 요소(구조체 포인터)의 크기로 나눠서 요소 개수를 구함
-    for (int i = 0; i < sizeof(tArr) / sizeof(tok_t *); i++)    // 요소 개수만큼 반복
+    for (int i = 0; i < sizeof(token_arr) / sizeof(tok_t *); i++)    // 요소 개수만큼 반복
     {
-        tArr[i] = malloc(sizeof(tok_t));    // 각 요소에 구조체 크기만큼 메모리 할당
+        token_arr[i] = malloc(sizeof(tok_t));    // 각 요소에 구조체 크기만큼 메모리 할당
     }
 
-    int startCursor = 0;
-    int endCursor;
-    int tsize = 0;
-    int ntok = 0; //mun of tok
+    int start_cursor = 0;
+    int end_cursor;
+    int token_size = 0;
+    int number_of_tokens = 0; //mun of tok
 
-    int cbcounter = 0;
-    int sbcounter = 0;
+    int cbracket_counter = 0;
+    int sbracket_counter = 0;
 
     for (int i = 1; i < length; i++) {
         // start index marked when data[i] != '\n' and ' ' and '\t'
         if(data[i] != ' ' && data[i] != '\n' && data[i] != '\t') {
-            startCursor = i;
+            start_cursor = i;
             // token string (mostly)
             if(data[i] == '\"') {
                 // check if it is \" character
                 if (data[i-1] != '\\'){
-                    startCursor++; //start index does not include first "
-                    for (int j = startCursor; ;j++){
+                    start_cursor++; //start index does not include first "
+                    for (int j = start_cursor; ;j++){
                         if (data[j] == '\"'){
-                            endCursor = j;
+                            end_cursor = j;
                             i = j; //i++ will be happen at upper "for" condition
                             break;
                         }
                     }
                     if (data[i+1] == ':'){
-                        if (cbcounter == 0 && sbcounter == 0) {
-                            tsize = 1;
-                            tArr[ntok]->start = startCursor;
-                            tArr[ntok]->end = endCursor;
-                            tArr[ntok]->size = tsize;
-                            tsize = 0;
-                            ntok++;
+                        if (cbracket_counter == 0 && sbracket_counter == 0) {
+                            token_size = 1;
+                            token_arr[number_of_tokens]->start = start_cursor;
+                            token_arr[number_of_tokens]->end = end_cursor;
+                            token_arr[number_of_tokens]->size = token_size;
+                            token_size = 0;
+                            number_of_tokens++;
                             i++;
                         }
                         else { // if it is in the object or array size +1
                             //inner object or array size does not count
-                            if (cbcounter < 2) tsize++;
+                            if (cbracket_counter < 2) token_size++;
                             //else nothing
                         }                        
                     }
                     else {
-                        if (cbcounter == 0 && sbcounter == 0) {
-                            // tsize = 0;
-                            tArr[ntok]->start = startCursor;
-                            tArr[ntok]->end = endCursor;
-                            tArr[ntok]->size = tsize;
-                            tsize = 0;
-                            ntok++;
+                        if (cbracket_counter == 0 && sbracket_counter == 0) {
+                            // token_size = 0;
+                            token_arr[number_of_tokens]->start = start_cursor;
+                            token_arr[number_of_tokens]->end = end_cursor;
+                            token_arr[number_of_tokens]->size = token_size;
+                            token_size = 0;
+                            number_of_tokens++;
                         } 
-                        // else if (sbcounter > 0) {
-                        else if (sbcounter == 1) {
+                        // else if (sbracket_counter > 0) {
+                        else if (sbracket_counter == 1) {
                             //name과 value가 페어가 아니라면
-                            if ((data[startCursor-2] != ':') || (data[startCursor-3] != ':') || (data[startCursor-4] != ':')) {
-                                tsize++;
+                            if ((data[start_cursor-2] != ':') || (data[start_cursor-3] != ':') || (data[start_cursor-4] != ':')) {
+                                token_size++;
                             }
                             // else 페어이기 때문에 페어일 경우 값을 가지지 않는다.
                         } //else nothing
@@ -128,40 +128,40 @@ int main() {
             }
 
             // object
-            else if (data[i] == '{' && sbcounter == 0){
-                // startCursor = i;
-                cbcounter++;
-                if (cbcounter == 1) {
-                    tArr[ntok]->start = startCursor;
+            else if (data[i] == '{' && sbracket_counter == 0){
+                // start_cursor = i;
+                cbracket_counter++;
+                if (cbracket_counter == 1) {
+                    token_arr[number_of_tokens]->start = start_cursor;
                 } // else nothing is counter > 1
             }
-            else if (data[i] == '}' && sbcounter == 0  && cbcounter > 0) {
-                cbcounter--;
-                if (cbcounter == 0) {
-                    tArr[ntok]->size = tsize; //
-                    tArr[ntok]->end = i+1; // +1?
-                    i = tArr[ntok]->start + 1;
-                    ntok++;
-                    tsize = 0;
+            else if (data[i] == '}' && sbracket_counter == 0  && cbracket_counter > 0) {
+                cbracket_counter--;
+                if (cbracket_counter == 0) {
+                    token_arr[number_of_tokens]->size = token_size; //
+                    token_arr[number_of_tokens]->end = i+1; // +1?
+                    i = token_arr[number_of_tokens]->start + 1;
+                    number_of_tokens++;
+                    token_size = 0;
                 } //else nothing
             }
 
             // array
-            else if (data[i] == '[' && cbcounter == 0) {
-                // startCursor = i;
-                sbcounter++;
-                if (sbcounter == 1) {
-                    tArr[ntok]->start = startCursor;
+            else if (data[i] == '[' && cbracket_counter == 0) {
+                // start_cursor = i;
+                sbracket_counter++;
+                if (sbracket_counter == 1) {
+                    token_arr[number_of_tokens]->start = start_cursor;
                 } // else nothing is counter > 1
             }
-            else if (data[i] == ']' && cbcounter == 0 && sbcounter > 0) {
-                sbcounter--;
-                if (sbcounter == 0) {
-                    tArr[ntok]->size = tsize; //
-                    tArr[ntok]->end = i+1;
-                    i = tArr[ntok]->start + 1;
-                    ntok++;
-                    tsize = 0;
+            else if (data[i] == ']' && cbracket_counter == 0 && sbracket_counter > 0) {
+                sbracket_counter--;
+                if (sbracket_counter == 0) {
+                    token_arr[number_of_tokens]->size = token_size; //
+                    token_arr[number_of_tokens]->end = i+1;
+                    i = token_arr[number_of_tokens]->start + 1;
+                    number_of_tokens++;
+                    token_size = 0;
                 } //else nothing
             }
 
@@ -175,29 +175,25 @@ int main() {
         }
     }
 
-    for (int i = 0; i < ntok; i++) {
+    for (int i = 0; i < number_of_tokens; i++) {
         printf("[%3d] ", i );
-        for (int j = tArr[i]->start; j < tArr[i]->end; j++ ) {
+        for (int j = token_arr[i]->start; j < token_arr[i]->end; j++ ) {
             printf("%c", data[j]);
         }
-        printf (" (size=%d, %d~%d)\n",tArr[i]->size, tArr[i]->start, tArr[i]->end);
+        printf (" (size=%d, %d~%d)\n",token_arr[i]->size, token_arr[i]->start, token_arr[i]->end);
         
     }
-
-
 
     printf("\nEnd of Program\n");
 
     free(data);
-    for (int i = 0; i < sizeof(tArr) / sizeof(struct tok_t *); i++)
+    for (int i = 0; i < sizeof(token_arr) / sizeof(struct tok_t *); i++)
     {
-        free(tArr[i]);
+        free(token_arr[i]);
     }
 
     return 0;
 }
-
-
 
 // Returns true (non-zero) if character-string parameter represents a signed or unsigned floating-point number. Otherwise returns false (zero).
 int isNumeric (const char * s)
