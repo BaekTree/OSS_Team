9edd444feaf7ad601a6c869a12757ec7d307ddf4
saintmin92 <tajiklee@naver.com>
2019-05-27 21:04:52 +0900

    type defining added

--- Sungmin/parser1.c
+++ Sungmin/parser1.c
@@ -18,20 +18,24 @@ typedef struct {
     int size; // Number of child (nested) tokens
 } tok_t;
 
-int main() {
+int main(int argc, char* argv[]) {
     FILE *fp;
     const int maxLen = 256;
     char buffer[maxLen];
     char *data = (char *)malloc(maxLen * sizeof(char));
 
     //file open
-    fp = fopen("test2.json", "r");
-    int length = 0; //total length
-    int num_of_str = 0; // number of strings
+    if (argc == 1) {        // if there is no argument
+        fp = fopen("test1.json", "r");
+    }
+    else {                  // get the file name from input
+        fp = fopen(argv[1], "r");
+    }
+    int length = 0;         // total length
+    int num_of_str = 0;     // number of strings
+    int ch;                 // character to read
 
-    // reading file by character
-    int ch;
-    // for (int i = 0; (i < (sizeof(buffer)-1) && ((ch = fgetc(fp)) != EOF) && (ch != '\n')); i++){
+    // 한 글자씩 읽어서 한 줄이 되면 data array에 넣고 버퍼는 초기화 
     for (int i = 0, j = 0; (ch = fgetc(fp)) != EOF; i++, j++){
         buffer[j] = ch;
         length++;
@@ -46,7 +50,6 @@ int main() {
     }
     buffer[length] = '\0';
     length++;
-
 
     // printf("The count is : \n%d\n", num_of_str);
     // printf("The length of string is : %d\n", length);
@@ -65,13 +68,13 @@ int main() {
         token_arr[i] = malloc(sizeof(tok_t));    // 각 요소에 구조체 크기만큼 메모리 할당
     }
 
-    int start_cursor = 0;
-    int end_cursor;
-    int token_size = 0;
-    int number_of_tokens = 0; //mun of tok
+    int start_cursor = 0;       // start index of the token
+    int end_cursor;             // end index of the token
+    int token_size = 0;         // size of token ( :pairs )
+    int number_of_tokens = 0;   // number of tokens
 
-    int cbracket_counter = 0;
-    int sbracket_counter = 0;
+    int cbracket_counter = 0;   // { } counter
+    int sbracket_counter = 0;   // [ ] counter
 
     for (int i = 1; i < length; i++) {
         // start index marked when data[i] != '\n' and ' ' and '\t'
@@ -89,28 +92,33 @@ int main() {
                             break;
                         }
                     }
+                    // if this token is "name"
                     if (data[i+1] == ':'){
                         if (cbracket_counter == 0 && sbracket_counter == 0) {
                             token_size = 1;
                             token_arr[number_of_tokens]->start = start_cursor;
                             token_arr[number_of_tokens]->end = end_cursor;
                             token_arr[number_of_tokens]->size = token_size;
+                            token_arr[number_of_tokens]->type = STRING;         //is STRING
                             token_size = 0;
                             number_of_tokens++;
                             i++;
                         }
-                        else { // if it is in the object or array size +1
+                        // if it is in the object or array size +1
+                        else { 
                             //inner object or array size does not count
                             if (cbracket_counter < 2) token_size++;
                             //else nothing
                         }                        
                     }
+                    // if token is string "value"
                     else {
                         if (cbracket_counter == 0 && sbracket_counter == 0) {
                             // token_size = 0;
                             token_arr[number_of_tokens]->start = start_cursor;
                             token_arr[number_of_tokens]->end = end_cursor;
                             token_arr[number_of_tokens]->size = token_size;
+                            token_arr[number_of_tokens]->type = STRING;         //is STRING
                             token_size = 0;
                             number_of_tokens++;
                         } 
@@ -129,17 +137,17 @@ int main() {
 
             // object
             else if (data[i] == '{' && sbracket_counter == 0){
-                // start_cursor = i;
                 cbracket_counter++;
                 if (cbracket_counter == 1) {
                     token_arr[number_of_tokens]->start = start_cursor;
-                } // else nothing is counter > 1
+                } // else nothing when counter > 1
             }
             else if (data[i] == '}' && sbracket_counter == 0  && cbracket_counter > 0) {
                 cbracket_counter--;
                 if (cbracket_counter == 0) {
                     token_arr[number_of_tokens]->size = token_size; //
                     token_arr[number_of_tokens]->end = i+1; // +1?
+                    token_arr[number_of_tokens]->type = OBJECT;     // is OBJECT
                     i = token_arr[number_of_tokens]->start + 1;
                     number_of_tokens++;
                     token_size = 0;
@@ -148,7 +156,6 @@ int main() {
 
             // array
             else if (data[i] == '[' && cbracket_counter == 0) {
-                // start_cursor = i;
                 sbracket_counter++;
                 if (sbracket_counter == 1) {
                     token_arr[number_of_tokens]->start = start_cursor;
@@ -165,11 +172,20 @@ int main() {
                 } //else nothing
             }
 
+            // 아래 두 벨류 토큰들은 어레이와 오브젝트 안에 있는지 밖에 있는지에 따라서 토크나이징을 할지 안할지 결정된다.
             // true false etc
-            else if (data[i] == '0'){
+            else if ((data[i] == 't' || data[i] == 'f')){  // Starts with t or f
+                // 오브젝트 안에 있을 때, 어레이 안에 있을 때, 그냥 벨류일 때
+                if (data[i] == 't') {
+
+                }
+                else if (data[i] == 'f') {
+
+                }
             }
             // numeric 
             else if (data[i]){
+                //첫 글자가 숫자인지 확인, 토큰 범위 찾고 숫자인지 확인, 숫자면 벨류로 지정하고 토크나이징
                 //double num = 1.234E+11;
             }
         }
