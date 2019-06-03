ab964f150c9a31e00e39f47b8f30ae2366d49a11
saintmin92 <tajiklee@naver.com>
2019-06-03 11:12:38 +0900

    visual modification

--- Sungmin/json_parser.c
+++ Sungmin/json_parser.c
@@ -4,6 +4,108 @@
 #include <ctype.h>
 #include "json_parser.h"
 
+// partial string to double
+double toDouble(char* s, int start, int stop);
+
+// print out the token array in form
+void print_array(int num_of_t, tok_t *t_arr, char *data);
+
+// filling the country array
+int country_maker(int num_of_tok, tok_t *t_arr, char *data, country_t *c_arr);
+
+// parsing the json file
+int json_parser(int length, int token_array_size, tok_t* token_arr, char* data);
+
+int main(int argc, char *argv[]) {
+    FILE *fp;
+    const int maxLen = 256;
+    char buffer[maxLen];
+    char *data = (char *)malloc(maxLen * sizeof(char));
+    int token_array_size = 1024;
+
+    //file open
+    if (argc == 1) {        // if there is no argument
+        fp = fopen("../Testfiles/countries.json", "r");
+    }
+    else {                  // get the file name from input
+        fp = fopen(argv[1], "r");
+    }
+    if(fp == NULL) {
+        printf("Reading failed");
+        return -1;
+    }
+    int length = 0;         // total length
+    char ch;                 // character to read
+
+    // 한 글자씩 읽어서 한 줄이 되면 data array에 넣고 버퍼는 초기화 
+    for (int i = 0, j = 0; (ch = fgetc(fp)) != EOF; i++, j++){
+        buffer[j] = ch;
+        length++;
+        if (ch == '\n' ){
+            // printf("%s\n", buffer);
+            data = (char *) realloc(data, strlen(data) + strlen(buffer));
+            strcat(data, buffer);
+            memset(buffer, 0, sizeof buffer);
+            j = -1;
+        }
+    }
+    if (buffer[0] != '\0') {
+        length++;
+        data = (char *) realloc(data, strlen(data) + strlen(buffer));
+        strcat(data, buffer);
+        memset(buffer, 0, sizeof buffer);
+        data[++length] = '\0';
+    }
+    fclose(fp);
+
+    //dynamic structure array
+    tok_t *token_arr;
+    token_arr = (tok_t *)malloc(sizeof(tok_t) * token_array_size);
+    for (int i = 0; i < token_array_size; i++) {
+        token_arr[i].type = UNDEFINED;
+    }
+
+    // parse and return number of tokens total
+    int num_of_token = json_parser(length, token_array_size, token_arr, data);       // number of tokens
+
+    // print out the token array
+    print_array(num_of_token, token_arr, data);
+
+    // declaring country array dynamically
+    country_t *country_arr;
+    country_arr = (country_t *)malloc(sizeof(country_t) * 100);
+
+    // fill array
+    int number_of_country = country_maker(num_of_token, token_arr, data, country_arr);
+
+    //print out the country array
+    /*
+    for (int i = 0; i < number_of_country; i++) {
+        printf("%s\n", country_arr[i].country_name);
+        printf("%s\n", country_arr[i].capital_name);
+        for (int j = 0; country_arr[i].language[j][0] != '\0'; j++){
+            printf("%s\n", country_arr[i].language[j]);
+        }
+        printf("%f\n", country_arr[i].gdp);
+        printf("%s\n", country_arr[i].population);
+        printf("%s\n", country_arr[i].currency);
+        printf("%s\n", country_arr[i].continent);
+        for (int j = 0; country_arr[i].legislature[j][0] != '\0'; j++){
+            printf("%s\n", country_arr[i].legislature[j]);
+        }
+    }
+    */
+
+    // memory free
+    free(data);
+    free(token_arr);
+    free(country_arr);
+
+    //return should be tok_t array for the application program
+    return 0;
+}
+
+
 double toDouble(char* s, int start, int stop) {
     char d_arr[16];
     double d;
@@ -89,7 +191,7 @@ int country_maker(int num_of_tok, tok_t *t_arr, char *data, country_t *c_arr){
     return num_of_con;
 }
 
-int parser(int length, int token_array_size, tok_t* token_arr, char* data) {
+int json_parser(int length, int token_array_size, tok_t* token_arr, char* data) {
     int num_of_token = 0;       // number of tokens
     int start_cursor = 0;       // start index of the token
     int end_cursor;             // end index of the token
@@ -256,93 +358,4 @@ int parser(int length, int token_array_size, tok_t* token_arr, char* data) {
         }
     }
     return num_of_token;
-}
-
-int main(int argc, char *argv[]) {
-    FILE *fp;
-    const int maxLen = 256;
-    char buffer[maxLen];
-    char *data = (char *)malloc(maxLen * sizeof(char));
-    int token_array_size = 1024;
-
-    //file open
-    if (argc == 1) {        // if there is no argument
-        fp = fopen("../Testfiles/countries.json", "r");
-    }
-    else {                  // get the file name from input
-        fp = fopen(argv[1], "r");
-    }
-    if(fp == NULL) {
-        printf("Reading failed");
-        return -1;
-    }
-    int length = 0;         // total length
-    char ch;                 // character to read
-
-    // 한 글자씩 읽어서 한 줄이 되면 data array에 넣고 버퍼는 초기화 
-    for (int i = 0, j = 0; (ch = fgetc(fp)) != EOF; i++, j++){
-        buffer[j] = ch;
-        length++;
-        if (ch == '\n' ){
-            // printf("%s\n", buffer);
-            data = (char *) realloc(data, strlen(data) + strlen(buffer));
-            strcat(data, buffer);
-            memset(buffer, 0, sizeof buffer);
-            j = -1;
-        }
-    }
-    if (buffer[0] != '\0') {
-        length++;
-        data = (char *) realloc(data, strlen(data) + strlen(buffer));
-        strcat(data, buffer);
-        memset(buffer, 0, sizeof buffer);
-        data[++length] = '\0';
-    }
-    fclose(fp);
-
-    //dynamic structure array
-    tok_t *token_arr;
-    token_arr = (tok_t *)malloc(sizeof(tok_t) * token_array_size);
-    for (int i = 0; i < token_array_size; i++) {
-        token_arr[i].type = UNDEFINED;
-    }
-
-    // parse and return number of tokens total
-    int num_of_token = parser(length, token_array_size, token_arr, data);       // number of tokens
-
-    // print out the token array
-    print_array(num_of_token, token_arr, data);
-
-    // declaring country array dynamically
-    country_t *country_arr;
-    country_arr = (country_t *)malloc(sizeof(country_t) * 100);
-
-    // fill array
-    int number_of_country = country_maker(num_of_token, token_arr, data, country_arr);
-
-    //print out the country array
-    /*
-    for (int i = 0; i < number_of_country; i++) {
-        printf("%s\n", country_arr[i].country_name);
-        printf("%s\n", country_arr[i].capital_name);
-        for (int j = 0; country_arr[i].language[j][0] != '\0'; j++){
-            printf("%s\n", country_arr[i].language[j]);
-        }
-        printf("%f\n", country_arr[i].gdp);
-        printf("%s\n", country_arr[i].population);
-        printf("%s\n", country_arr[i].currency);
-        printf("%s\n", country_arr[i].continent);
-        for (int j = 0; country_arr[i].legislature[j][0] != '\0'; j++){
-            printf("%s\n", country_arr[i].legislature[j]);
-        }
-    }
-    */
-
-    // memory free
-    free(data);
-    free(token_arr);
-    free(country_arr);
-
-    //return should be tok_t array for the application program
-    return 0;
 }
