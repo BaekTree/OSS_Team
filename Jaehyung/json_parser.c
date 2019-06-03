9c77e75da1c878808f939146e66052bd8dbe91c2
BaekTree <etknbe@gmail.com>
2019-06-03 13:11:56 +0900

    integ: add functions

--- Jaehyung/json_parser.c
+++ Jaehyung/json_parser.c
@@ -3,6 +3,15 @@
 #include <string.h>
 #include <ctype.h>
 #include "json_parser.h"
+void Options();
+void sortABC(country_t *ctr, int n);
+void sortGDP(country_t *ctr, int n);
+void printRank(country_t *rk, int n);
+void rank(country_t *ctr, int n);
+void ABCD(country_t *ctr, int n);
+void Quiz(country_t *country);
+void Search_Continent(country_t *cty, char *search);
+void Search_Country(country_t *cty, char *search);
 
 // partial string to double
 double toDouble(char* s, int start, int stop);
